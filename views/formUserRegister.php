43934fd760be43bed450a010e2d393ca54c5ad2f
Tam <Tam@Tam-PC>
2015-02-18 14:06:08 +0100

    change to english

---
+++ views/formUserRegister.php
@@ -0,0 +1,18 @@
+<html
+    <head>
+        <title>User registration</title>
+        <meta charset='utf-8'>
+        <link rel="stylesheet" href="../style.css">
+    </head>
+    <body>
+        <form method='post' action='index.php'>
+            <h1>User registration: </h1>
+            Name: <input type='text' name='name'/></br></br>
+            Surname: <input type='text' name='surname'/></br></br>
+            Id: <input type='text' name='id'/></br></br>
+            Age: <input type='text' name='age'/></br></br></br>
+        
+            <input type="submit" value='Send'/>
+        </form>
+    </body>
+</html>
